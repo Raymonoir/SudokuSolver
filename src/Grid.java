013f5289a8e57e82e82c77c4a6240ad79a0d16e7
Raymond Ward <rayward1337@gmail.com>
2019-12-21 13:50:29 +0000

    Grid class is now able to form a grid on GridCells, each cell is checked if it was given originally and the given variable is set accordingly. Also added various getters and setters for each class

--- src/Grid.java
+++ src/Grid.java
@@ -1,4 +1,81 @@
 
-public class Grid {
+/**
+ * 
+ * @author raymonoir
+ * 
+ * 
+ * BACKTRACKING ALGORITHM
+ * 
+ * if 0, increment
+ * does 1 work, if yes move on,
+ * if not keep incrementing 
+ * 
+ * if no values work, backtrack 
+ * 
+ * 
+ *
+ */
+
+
+
+
+
+
+
+public class Grid 
+{
+	GridCell [][] gridArray;
+	int [][] numberGrid;
+	
+	
+	Grid(int [][] sudoku)
+	{
+		int [][] numberGrid = sudoku;
+		formGridArray();
+		
+	}
+	
+	
+	private void formGridArray()
+	{
+		gridArray  = new GridCell [9][9];
+		
+		for (int i = 0; i < numberGrid.length; i ++)
+		{
+			for (int j = 0; j < numberGrid.length; j ++)
+			{
+				gridArray[i][j] = new GridCell(numberGrid[i][j]);
+				
+			}
+			
+		}
+		
+	}
+	
+	private boolean checkRow (int i, int j)
+	{
+		return false;
+	}
+	
+	private boolean checkColumn (int i, int j)
+	{
+		return false;
+		
+	}
+	
+	private boolean checkBox (int i, int j)
+	{
+		return false;
+	}
+	
+	
+	private void backtrack (int i, int j)
+	{
+		if (gridArray[i][j].getVal() == 0)
+			gridArray[i][j].incVal();
+		
+		if ()
+		
+	}
 
 }
--- src/GridCell.java
+++ src/GridCell.java
@@ -2,6 +2,35 @@
 public class GridCell 
 {
 	private int value;
+	private boolean given;
 	
+	
+	GridCell(int val)
+	{
+		value = val;
+	}
+	
+	
+	
+	public int getVal ()
+	{
+		return value;
+	}
+	
+	public void incVal()
+	{
+		value++;
+	}
+	
+	
+	public boolean getGiven ()
+	{
+		return given;
+	}
+	
+	public void setGiven (boolean giv)
+	{
+		given = giv;
+	}
 
 }
